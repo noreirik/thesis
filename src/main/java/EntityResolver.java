4605366d1dfdee737379bb634013f19ce8b073cf
Eirik S. Norheim <noreirik@gmail.com>
2017-01-15 23:32:39 +0100

    Implemented pleliminary entity resolver based on R-Swoosh

--- src/main/java/EntityResolver.java
+++ src/main/java/EntityResolver.java
@@ -1,17 +1,43 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class EntityResolver {
 
 	private boolean match(Entity x, Entity y) {
-		return false;
+		if (x == null) return x == y;
+		if (y == null) return false;
+		return x.getId().toLowerCase().equals(y.getId().toLowerCase());
 	}
 	
+	// TODO: Actual merging
 	private Entity merge(Entity x, Entity y) {
-		return null;
+		return x;
 	}
 	
-	public List<Entity> resolve(List<Entity> entities) {
-		return null;
+	// Generic R-Swoosh algorithm 
+	public List<Entity> resolve(List<Entity> d2) {
+		List<Entity> er = new ArrayList<Entity>();
+		List<Entity> d = new ArrayList<Entity>(d2);
+		
+		while (!d.isEmpty()) {
+			Entity currentRecord = d.iterator().next();
+			d.remove(currentRecord);
+			Entity buddy = null;
+			for (Entity r : er) {
+				if (match(currentRecord, r)) {
+					buddy = r;
+					break;
+				}
+			}
+			if (buddy == null) {
+				er.add(currentRecord);
+			} else {
+				Entity merged = merge(currentRecord, buddy);
+				er.remove(buddy);
+				d.add(merged);
+			}
+		}
+		return er;
 	}
 	
 }
