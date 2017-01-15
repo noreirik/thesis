5cd43c56059cbcbc0375feed9340e99c135140ad
Eirik S. Norheim <noreirik@gmail.com>
2017-01-16 00:38:07 +0100

    Refactored Driver. Moved methods to separate classes

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,44 +4,30 @@ import java.util.List;
 
 public class Driver {
 	
-	private List<String[]> records;
-	
 	public static void main(String[] args) {
-		
-		Driver driver = new Driver();
-		driver.records = new ArrayList<String[]>();
 		
 		String datadir; // Data set directory, TODO: Move to configuration file probably
 		datadir = args.length == 1 ? args[0] : "/home/eirik/Dropbox/Universitetsarbeid/thesis/datasets/workingset";
 		File dir = new File(datadir);
 		
-		CSVParser parser = new CSVParser();
+		EntityParser parser = new EntityParser();
+		List<Entity> entities = new ArrayList<Entity>();
+		
 		for (File f : dir.listFiles()) {
 			if (f.isFile() && f.getName().toLowerCase().endsWith("csv")) {
-				List<String[]> currentRecords = parser.parse(f.getAbsolutePath());
-				driver.insertRecords(currentRecords);
+				entities.addAll(parser.parse(f.getAbsolutePath()));
 			}
 		}
+		
+		EntityResolver resolver = new EntityResolver();
+		List<Entity> resolvedEntities = resolver.resolve(entities);
 		
 		System.exit(0);
 	}
 	
-	private void insertRecords(List<String[]> records) {
-		// TODO: Create Entity objects from strings and insert into records
-		// if both firstName and lastName fields are set, use that as the best representation of a full name
-	}
-	
-	private void printRecords() {
-		for (int i = 0; i < records.size(); i++) {
-			String[] record = records.get(i);
-			for (int j = 0; j < record.length; j++) {
-				if (!record[j].isEmpty())
-					System.out.print(record[j]);
-				else
-					System.out.print("NULL");
-				if (j != record.length-1) System.out.print(":");
-			}
-			System.out.println();
+	private static void printRecords(List<Entity> l) {
+		for (int i = 0; i < l.size(); i++) {
+			System.out.println(l.get(i));
 		}
 	}
 }
