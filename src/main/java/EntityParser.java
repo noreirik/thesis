ba58b000962c8d85d9e58b3edb651a1d46308517
Eirik S. Norheim <noreirik@gmail.com>
2017-01-16 00:35:40 +0100

    Use EntityParser instead of calling CSV reader directly

---
+++ src/main/java/EntityParser.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityParser {
+
+	private ERCSVReader reader;
+	
+	public EntityParser() {
+		reader = new ERCSVReader();
+	}
+	
+	public List<Entity> parse(String filename) {
+		List<String[]> records = reader.read(filename);
+		List<Entity> entities = retrieveRecords(records);
+		return entities;
+	}
+	
+	private int idIdx, nameIdx, lastNameIdx, firstNameIdx,  jobTitleIdx;
+	
+	private List<Entity> retrieveRecords(List<String[]> records) {
+		List<Entity> entities = new ArrayList<Entity>();
+		
+		String[] header = records.get(0);
+		updateIndices(header);
+		
+		for (int i = 1; i < records.size(); i++) {
+			String[] record = records.get(i);
+			entities.add(createEntity(record));
+		}
+		return entities;
+	}
+	
+	private Entity createEntity(String[] record) {
+		String id, name, firstName, lastName, jobTitle;
+		id = name = firstName = lastName = jobTitle = "";
+		if (idIdx != -1) 		id = record[idIdx];
+		if (nameIdx != -1) 		name = record[nameIdx];
+		if (lastNameIdx != -1)	lastName = record[lastNameIdx];
+		if (firstNameIdx != -1) firstName = record[firstNameIdx];
+		if (jobTitleIdx != -1)	jobTitle = record[jobTitleIdx];
+		
+		return new	EntityBuilder()
+					.id(id)
+					.fullName(name)
+					.firstName(firstName)
+					.lastName(lastName)
+					.jobTitle(jobTitle)
+					.build();
+	}
+	
+	private void updateIndices(String[] header) {
+		idIdx = nameIdx = lastNameIdx = firstNameIdx = jobTitleIdx = -1;
+		for (int idx = 0; idx < header.length; idx++) {
+			switch(header[idx].toLowerCase()) {
+				case "last name":  	lastNameIdx = idx; break;
+				case "first name":  firstNameIdx = idx; break;
+				case "name": 		nameIdx = idx; break;
+				case "title": 		jobTitleIdx = idx; break;
+				case "id": 			idIdx = idx; break;
+				default: break;
+			}
+		}
+	}
+}
