37661f0bc3c8869bf4ba50eb73a276fa6a4ab4ee
Eirik S. Norheim <noreirik@gmail.com>
2017-01-15 23:06:24 +0100

    Added separate class as base for resolving references

---
+++ src/main/java/EntityResolver.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class EntityResolver {
+
+	private boolean match(Entity x, Entity y) {
+		return false;
+	}
+	
+	private Entity merge(Entity x, Entity y) {
+		return null;
+	}
+	
+	public List<Entity> resolve(List<Entity> entities) {
+		return null;
+	}
+	
+}
