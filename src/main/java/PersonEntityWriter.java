d93faa21c5611afa51e98419bb8629f655dbdba5
Eirik S. Norheim <noreirik@gmail.com>
2017-01-16 22:16:57 +0100

    Print references with defined first and last names last

--- src/main/java/PersonEntityWriter.java
+++ src/main/java/PersonEntityWriter.java
@@ -10,7 +10,6 @@ public class PersonEntityWriter extends EntityWriter {
 		super.open();
 	}
 
-	// only writes entities where first name or last name is defined for now
 	@Override
 	public void write(Entity entity) {
 		String firstName = ""; String lastName = "";
@@ -22,17 +21,24 @@ public class PersonEntityWriter extends EntityWriter {
 		}
 		if (!entity.getLastName().isEmpty()) {
 			lastName = entity.getLastName();
+			defined = true;
 		}
+		String s = "";
 		if (defined) {
 			// create_entity PERSON PERSON_FIRST_NAME Gunilla "Gunilla Andreasson" PERSON_LAST_NAME "Andreasson Steen" Steen
-			String s = String.format("create_entity PERSON PERSON_FIRST_NAME " +
+			s = String.format("create_entity PERSON PERSON_FIRST_NAME " +
 									"%s PERSON_LAST_NAME %s\n", firstName, lastName);
-			this.output.add(s);
+		} else {
+			s = String.format("create entity PERSON PERSON_NAME %s\n", entity.getFullName());
+			defined = false;
 		}
+		if (defined) this.output.add(s);
+		else this.output.add(0, s);
 	}
 	
 	@Override
 	public void close() {
+		this.output.add("\n");
 		this.output.add("# final clean up to merge any duplicates\n");
 		this.output.add("cleanup_entities");
 		this.output.add("\n");
