596507921cb247c7f441d1d6567d9734989cb729
Eirik S. Norheim <noreirik@gmail.com>
2017-01-18 22:18:30 +0100

    Added custom equality testing for entities

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -54,5 +54,68 @@ public class Entity {
 	
 	public boolean verify(String s) {
 		return !(s.isEmpty() || s == null);
-	}	
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((jobTitle == null) ? 0 : jobTitle.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) { return true; }
+		if (obj == null) { return false; }
+		if (!(obj instanceof Entity)) { return false; }
+		Entity other = (Entity) obj;
+		if (firstName == null) {
+			if (other.firstName != null) {
+				return false;
+			}
+		} else if (!firstName.toLowerCase().equals(other.firstName.toLowerCase())) {
+			return false;
+		}
+		if (fullName == null) {
+			if (other.fullName != null) {
+				return false;
+			}
+		} else if (!fullName.toLowerCase().equals(other.fullName.toLowerCase())) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.toLowerCase().equals(other.id.toLowerCase())) {
+			return false;
+		}
+		if (jobTitle == null) {
+			if (other.jobTitle != null) {
+				return false;
+			}
+		} else if (!jobTitle.equals(other.jobTitle.toLowerCase())) {
+			return false;
+		}
+		if (lastName == null) {
+			if (other.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.toLowerCase().equals(other.lastName.toLowerCase())) {
+			return false;
+		}
+		return true;
+	}
+
 }
